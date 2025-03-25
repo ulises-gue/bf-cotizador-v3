bd1b4f256ef70246bd9eed4c36862f7c007f0899
ulises-gue <111939337+ulises-gue@users.noreply.github.com>
2025-03-25 13:48:03 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -122,8 +122,8 @@ km = {"Aguascalientes - Chihuahua": 1310.18181818182,
 "San Luis Potosi - Tlaxcala": 510.6}
 
 #We will prompt the user to upload a file
-uploaded_file = st.file_uploader("Sube la contrapropuesta del cliente", type = ["xlsx"], key = "file1")
-uploaded_file2 = st.file_uploader("Sube la cotizacion original", type = ["xlsx"], key = "file2")
+uploaded_file = st.file_uploader("Sube la contrapropuesta del cliente:", type = ["xlsx"], key = "file1")
+uploaded_file2 = st.file_uploader("Sube la cotizacion original:", type = ["xlsx"], key = "file2")
 
 #The application will run the program only if the user uploadas a file, otherwise it will display
 #an error message.
