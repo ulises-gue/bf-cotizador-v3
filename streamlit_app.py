74ad550d2ab5ff69604cf46360f89f8c04ca473f
ulises-gue <111939337+ulises-gue@users.noreply.github.com>
2025-03-25 11:44:56 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,6 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+
+st.title("Border Freight - Cotizador de Rutas")
+st.write("Este cotizador esta diseñado para subir un archivo de rutas y evaluar los precios que contiene")
